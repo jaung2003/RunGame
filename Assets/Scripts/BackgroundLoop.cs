48078301a6ad08cab58027c2c7eb244fd198c307
jaung2003 <80864723+jaung2003@users.noreply.github.com>
2021-04-08 10:16:59 +0900

    0408 강민우

---
+++ Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLoop : MonoBehaviour
+{
+    private float width;
+    void Awake()
+    {
+        BoxCollider2d backgroundCollider = GetCompenent<BoxCollider2d>();
+        width = backgroundCollider.size.x; //2,48
+    }
+
+    
+    void Update()
+    {
+        if(transform.posittion.x <= -width)
+    }
+}
+    private void Reposittion()
+    {
+    Vector2 offset = new Vector2(width * 2f, 0);
+    transform.posittion = (vector2)transform.posittion + offset;
+    }
+}
